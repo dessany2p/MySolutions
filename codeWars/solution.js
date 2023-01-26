18965de4fd2772b3f2ace1b821a018400df80a2e
mr. Green <roopxh@gmail.com>
2023-01-26 23:23:18 +0600

    changes2

--- codeWars/solution.js
+++
@@ -1,9 +0,0 @@
-function solution(str, ending) { 
-  const a = str;
-  const lenE = ending.length;
-  let x = a.slice(-lenE) === ending;
-  if (ending === '') {
-    x = a.slice(lenE) !== ending;
-  }
-  return x;
-}

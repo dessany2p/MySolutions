b8c3344486aa11c725aadc2f43d4fa53ee9b8b25
mr. Green <roopxh@gmail.com>
2023-01-30 16:09:07 +0600

    Delete one line in solution.js , final fixed

--- solution.js
+++ solution.js
@@ -1,6 +1,5 @@
 function solution(str, ending) {
   const endLength = ending.length;
-  let x = str.slice(-endLength) === ending;
   return ending === '' ? true : str.slice(-endLength) === ending;
 };
 
